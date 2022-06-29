a6195c31b5848fd55cfa069699a7470d7c33c88a
amerh12 <91185105+amerh12@users.noreply.github.com>
2022-06-29 17:48:28 +0200

    Add files via upload

---
+++ Krog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNaloga3
+{
+    class Krog
+    {
+        public double _polmer;
+       
+
+
+
+        //premer=polmer*2;
+        //povrsina = polmer*polmer*pi
+        //obseg=2*3.14*premer
+
+
+        public double Polmer
+        {
+            get { return _polmer; }
+            set { _polmer = value; }
+        }
+
+
+        public double Obseg(double polmer)
+        {
+            
+            double obseg = 2*3.14*(2*polmer);
+            return obseg;
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNaloga3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Krog k1 = new Krog();
+            Krog k2 = new Krog();
+            Random rd = new Random();
+            
+            double k1_double = k1.Obseg(20);
+            double k2_double = k2.Obseg(rd.Next(5,32));
+
+            if(k1_double > k2_double)
+            {
+                Console.WriteLine("k1 ({0}) je vecji od k2 ({1}) ", k1_double, k2_double);
+
+
+            }
+            else
+            {
+                Console.WriteLine("k2 ({0}) je vecji od k1 ({1}) ", k2_double, k1_double);
+            }
+
+        }
+    }
+}
